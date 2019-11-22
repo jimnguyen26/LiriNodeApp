32632ceb37f3be93774012ca80fa0021197b9775
Jim <jimmybnguyen@yahoo.com>
2019-11-22 14:33:41 -0800

    logs artist

--- liri.js
+++ liri.js
@@ -1,8 +1,12 @@
 require("dotenv").config();
 
 const keys = require("./keys");
+const axios = require('axios');
 
-console.log(keys);
+// console.log(keys);
 
-const spotify = new Spotify(keys.spotify);
+// const spotify = new Spotify(keys.spotify);
 
+const url = "https://rest.bandsintown.com/artists/celine+dion/events?app_id=codingbootcamp";
+
+axios.get(url).then(res => console.log(res.data[0].venue.name));
