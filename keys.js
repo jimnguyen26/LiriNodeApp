2121d6488559cab9d3cbcce8bd2131dc9da1ba8b
Jim <jimmybnguyen@yahoo.com>
2019-11-27 16:29:37 -0800

    artist/spotify added

--- keys.js
+++ keys.js
@@ -1,18 +1,17 @@
 // console.log('this is loaded');
 
-const keys = () => {
-    const arguments = process.argv;
-    let argArr = [];
-
-    for (let i = 2; i < arguments.length; i++) {
-        argArr.push(arguments[i]);
-    }
-    return argArr.join("+");
-};
-
 exports.spotify = {
     id: process.env.SPOTIFY_ID,
     secret: process.env.SPOTIFY_SECRET
 };
 
-module.exports = keys;
+// const keys = () => {
+//     const arguments = process.argv;
+//     let argArr = [];
+
+//     for (let i = 2; i < arguments.length; i++) {
+//         argArr.push(arguments[i]);
+//     }
+//     return argArr.join("+");
+// };
+// module.exports = keys;
--- liri.js
+++ liri.js
@@ -1,19 +1,81 @@
 require("dotenv").config();
 
-const keys = require("./keys");
+const keys = require("./keys.js");
 const axios = require('axios');
+const Spotify = require('node-spotify-api');
+const spotify = new Spotify(keys.spotify);
+const spotifyTest = process.env.SPOTIFY_ID
 const moment = require('moment');
 moment().format();
 
-const showDate = moment().format();
 
-// console.log(keys);
+let category = process.argv[2]
+let searchTitle = process.argv[3]
 
-const movieTitle = require("./keys")
-// const spotify = new Spotify(keys.spotify);
+switch (category) {
+    case "concert-this":
+        searchForBands(searchTitle);
+        break;
+    case "spotify-this-song": 
+        spotifySong(searchTitle);
+        break;
+    case "movie-this":
+        movieThis(searchTitle);
+        break;
+    case "do-what-it-says":
+        doRandom();
+    break;
+}
 
-const url = `https://rest.bandsintown.com/artists/${movieTitle()}/events?app_id=codingbootcamp`;
+function searchForBands(artist) {
+    const queryUrl = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+    axios.get(queryUrl).then(
+        (res => {
+            if(res.data[0].venue !=  undefined) {
+                console.log("Venue: " + res.data[0].venue.name);
+                console.log("Location: " + res.data[0].venue.city);
+                console.log(moment(res.data[0].datetime).format("MM-DD-YYYY"));
+            }
+            else {
+                console.log("No results found.");
+            }
+        })
+    ).catch(error => {
+        console.log(error);
+  });
+}
 
-axios.get(url).then(res => console.log(res.data[0].venue.name));
-axios.get(url).then(res => console.log(res.data[0].venue.city));
-axios.get(url).then(res => console.log(moment(res.data[0].datetime).format("MM-DD-YYYY")));
+function spotifySong(song) {
+    spotify
+    .search({ type: 'track', query: song })
+    .then(res => {
+        if (res.tracks.total === 0) {
+            errorSpotify();
+        } else {
+            console.log("Artist: " + res.tracks.items[0].artists[0].name);
+            console.log("Song Title: " + res.tracks.items[0].name);
+            console.log("Spotify URL: " + res.tracks.items[0].preview_url);
+            console.log("Album: " + res.tracks.items[0].album.name);
+        }
+    }).catch(error => {  
+        console.log(error);
+    });
+}
+
+function errorSpotify() {
+    spotify
+    .search({ type: 'track', query: 'The Sign' })
+    .then(res => {
+        for (var i=0;i < res.tracks.items.length; i++) {
+            if (res.tracks.items[i].artists[0].name === "Ace of Base") {
+                console.log("Artist: " + res.tracks.items[i].artists[0].name);
+                console.log("Song Title: " + res.tracks.items[i].name);
+                console.log("Spotify URL: " + res.tracks.items[i].preview_url);
+                console.log("Album: " + res.tracks.items[i].album.name);
+                i = res.tracks.items.length;
+            }
+        }
+    }).catch(error => {  
+        console.log(error);
+    });
+}
